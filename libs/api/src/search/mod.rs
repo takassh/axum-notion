d26a531d680d9ad9459fcdcf71b08d6a960e0307
Takashi Kasai <takassh23@gmail.com>
2024-05-16 23:46:17 +0900

    improve performance

--- libs/api/src/search/mod.rs
+++ libs/api/src/search/mod.rs
@@ -12,11 +12,14 @@ use cloudflare::models::{
     },
 };
 use futures_util::{pin_mut, Stream};
+
 use qdrant_client::qdrant::{
     with_payload_selector::SelectorOptions, PayloadIncludeSelector,
     SearchPoints, WithPayloadSelector,
 };
+use serde_json::json;
 use std::{convert::Infallible, sync::Arc};
+use tokio::{select, sync::mpsc};
 use tokio_stream::StreamExt as _;
 use tracing::error;
 
@@ -84,93 +87,151 @@ pub async fn search_text_with_sse(
     Json(params): Json<SearchParam>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let stream = stream! {
-    let result = retriever(&state, &params.prompt).await;
-    let Ok((context,page_ids)) = result else {
-        error!(
-            task = "get context by retriever",
-            error = result.unwrap_err().to_string(),
-        );
-        return;
-    };
+            let result = retriever(&state, &params.prompt).await;
+            let Ok((context,page_ids)) = result else {
+                error!(
+                    task = "get context by retriever",
+                    error = result.unwrap_err().to_string(),
+                );
+                return;
+            };
 
-    let system_prompt = r#"
+            let system_prompt = r#"
         You are an assistant helping a user who gives you a prompt.
         You are placed on my blog site.
         Each time the user gives you a prompt, you get information and page ids relating to the prompt.
-        Referencing them and using your knowledge, you respond to the given prompt.
         If you aren't familiar with the prompt, you should answer you don't know.
         "#.to_string();
 
-    let user_prompt = format!(
-        r#"
+            let user_prompt = format!(
+                r#"
         Prompt: 
         "{}"
         Information: 
         "{}"
-        Page IDs: 
-        "{}"
         "#,
-        params.prompt,
-        context.join("\n"),
-        page_ids.join(",")
-    );
+                params.prompt,
+                context.join("\n")
+            );
 
-    let mut messages = params.history;
-    messages.insert(0,Message {
-        role: "system".to_string(),
-        content: system_prompt.to_string(),
-    });
-    messages.insert(1,Message {
-        role: "user".to_string(),
-        content: r#"
+            let mut messages = params.history;
+            messages.insert(0,Message {
+                role: "system".to_string(),
+                content: system_prompt.to_string(),
+            });
+            messages.insert(1,Message {
+                role: "user".to_string(),
+                content: r#"
         Prompt: 
         "Hello, What can you help me?"
         Information: 
         You are an assistant helping a user to search for something.
         You are created by Takashi, who is a software engineer and the owner where you are placed.
-        Page IDs:
-        74c5e456-0feb-4049-a217-ba6ad67869ca,74c5e456-0feb-4049-a217-ba6ad67869ca
         "#.to_string(),
-    });
-    messages.insert(2,Message {
-        role: "assistant".to_string(),
-        content: r#"
-        {
-            "answer": "Hello, I can help you with searching. And I'm created by Takashi. He is a software engineer and the owner of this site.",
-            "page_ids": ["74c5e456-0feb-4049-a217-ba6ad67869ca","74c5e456-0feb-4049-a217-ba6ad67869ca"]
-        }
+            });
+            messages.insert(2,Message {
+                role: "assistant".to_string(),
+                content: r#"
+        Hello, I can help you with searching. And I'm created by Takashi. He is a software engineer and the owner of this site.
         "#.to_string(),
-    });
-    messages.push(Message {
-        role: "user".to_string(),
-        content: user_prompt.to_string(),
-    });
+            });
+            messages.push(Message {
+                role: "user".to_string(),
+                content: user_prompt.to_string(),
+            });
 
-    let response = state
-    .cloudflare
-    .llama_3_8b_instruct_with_stream(TextGenerationRequest::Message(
-        MessageRequest {
-            messages,
-            stream: Some(true),
-            ..Default::default()
-        },
-    ));
 
-        pin_mut!(response); // needed for iteration
 
-        // receive response from LLM
-        while let Ok(Some(data)) = response.next().await.transpose() {
-            for d in data{
-                let event =  Event::default().json_data(d);
-                match event {
-                    Ok(event) => yield event,
-                    Err(err) => error!(
-                        task = "event json_data",
-                        error = err.to_string()
-                    ),
+                let (message_tx, mut message_rx) = mpsc::channel(100);
+                let (page_tx, mut page_rx) = mpsc::channel(1);
+
+                // Receive messages from LLM
+                let _state = state.clone();
+                tokio::spawn(async move{
+                    let response = _state
+                    .cloudflare
+                    .llama_3_8b_instruct_with_stream(TextGenerationRequest::Message(
+                        MessageRequest {
+                            messages,
+                            stream: Some(true),
+                            ..Default::default()
+                        },
+                    ));
+                    pin_mut!(response); // needed for iteration
+                    while let Ok(Some(data)) = response.next().await.transpose() {
+                        for d in data{
+                            let event =  Event::default().json_data(json!({"message":d.response}));
+
+                            match event {
+                                Ok(event) => {
+                                    let result = message_tx.send(event).await;
+                                    if let Err(err) = result {
+                                        error!(
+                                            task = "send message event",
+                                            error = err.to_string()
+                                        );
+                                    }
+                                },
+                                Err(err) => error!(
+                                    task = "event json_data",
+                                    error = err.to_string()
+                                ),
+                            }
+                        }
+                    }
+                });
+
+                // Take pages from page ids
+                tokio::spawn(async move{
+                    let mut pages = vec![];
+                    for id in &page_ids{
+                        let result = state.repo.page.find_by_id(id).await;
+                        let Ok(page) = result else {
+                            error!(
+                                task = "get page by notion client",
+                                error = result.unwrap_err().to_string(),
+                            );
+                            continue;
+                        };
+
+                        let Some(page) = page else{
+                            continue;
+                        };
+
+                        pages.push(page.contents);
+                    }
+
+                    let event = Event::default().json_data(json!({"pages":pages}));
+                    match event {
+                        Ok(event) => {
+                            let result = page_tx.send(event).await;
+                            if let Err(err) = result {
+                                error!(
+                                    task = "send message event",
+                                    error = err.to_string()
+                                );
+                            }
+                        },
+                        Err(err) => error!(
+                            task = "event json_data",
+                            error = err.to_string()
+                        ),
+                    };
+                });
+
+        loop {
+            select! {
+                Some(event) = message_rx.recv() => {
+                    yield event;
+                }
+                Some(event) = page_rx.recv() => {
+                    yield event;
+                }
+                else => {
+                    break;
                 }
             }
-        }
+        };
     };
 
     Sse::new(stream.map(Ok))
